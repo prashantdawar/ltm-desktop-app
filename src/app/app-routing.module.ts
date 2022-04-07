96ca57b62a36191fc7112e195e5399def5b1e6df
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-07 19:35:54 +0530

    Added: Electron Forge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { CouponDetailsFormComponent } from './coupon-details-form/coupon-details-form.component';
 import { CouponDetailsIndexComponent } from './coupon-details-index/coupon-details-index.component';
 import { CustomerDetailsFormComponent } from './customer-details-form/customer-details-form.component';
@@ -7,17 +8,19 @@ import { CustomerDetailsIndexComponent } from './customer-details-index/customer
 import { ProductDetailsFormComponent } from './product-details-form/product-details-form.component';
 import { ProductDetailsIndexComponent } from './product-details-index/product-details-index.component';
 
-
-
-
 const routes: Routes = [
+  { path: '/', component: AppComponent },
+  
+  
   { path: 'customer-details', component: CustomerDetailsIndexComponent },
   { path: 'customer-details/create', component: CustomerDetailsFormComponent },
 
+  
   { path: 'product-details', component: ProductDetailsIndexComponent },
   { path: 'product-details/create', component: ProductDetailsFormComponent },
 
 
+  
   { path: 'coupon-details', component: CouponDetailsIndexComponent },
   { path: 'coupon-details/create', component: CouponDetailsFormComponent }
 ];
