ab59939a9e9120d12297fae2ab9957507f3b7c12
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-14 08:17:53 +0530

    udated

--- src/app/customer-details-form/customer-details-form.component.ts
+++ src/app/customer-details-form/customer-details-form.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
 import { CustomerDetails } from '../modals_data/customer_details';
+
 @Component({
   selector: 'app-customer-details-form',
+
+
+
   templateUrl: './customer-details-form.component.html',
   styleUrls: ['./customer-details-form.component.css']
 })
 
-
 export class CustomerDetailsFormComponent implements OnInit {
   model: any = new CustomerDetails();
-  breadcrumbItems: any[] = [];
 
+
+  breadcrumbItems: any[] = [];
   constructor(private dbService: NgxIndexedDBService) { }
 
-
-
   ngOnInit(): void {
+    
     this.breadcrumbItems = [{
       'label': 'Home',
       'urlTo': '',
@@ -34,9 +37,6 @@ export class CustomerDetailsFormComponent implements OnInit {
     // this.model.customer_name = "test customer name";
   }
 
-
-
-
   onSubmit(form: NgForm): void {
     console.log('submit');
     // this.model.save();
@@ -45,14 +45,17 @@ export class CustomerDetailsFormComponent implements OnInit {
     const data = form.value;
     this.model.beforeSave();
     if (form.status == "VALID") {
+   
+   
+   
       this.dbService
-        .add('customer_details', this.model)
+        .add('customer_details', this.model)   
         .subscribe((key) => {
           console.log('key: ', key);
 
-          // form.reset(); // only working in template-driven form for fixed template.
-          // this.model = null;
-          // this.model = new CustomerDetails();
+          form.reset(); // only working in template-driven form for fixed template.
+          this.model = null;
+          this.model = new CustomerDetails();
         });
     } else {
       console.log("Fill Fields");
