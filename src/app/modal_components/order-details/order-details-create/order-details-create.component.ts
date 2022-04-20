1e9e9b3ec08008910d59a6b827ac70ebb037fa6c
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-20 19:48:33 +0530

    updated

--- src/app/modal_components/order-details/order-details-create/order-details-create.component.ts
+++ src/app/modal_components/order-details/order-details-create/order-details-create.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { OrderDetails } from 'src/app/modals_data/order_details';
 
 @Component({
   selector: 'app-order-details-create',
   templateUrl: './order-details-create.component.html',
   styleUrls: ['./order-details-create.component.css']
 })
-export class OrderDetailsCreateComponent implements OnInit {
 
-  constructor() { }
+
+export class OrderDetailsCreateComponent implements OnInit {
+  model: any = new OrderDetails();
+
+
+  breadcrumbItems: any[] = [];
+  constructor(private route: ActivatedRoute, private dbService: NgxIndexedDBService) { }
 
   ngOnInit(): void {
+    this.breadcrumbItems = [{
+      'label': 'Home',
+      'urlTo': '',
+    },
+    {
+      'label': this.model.getClassLabel(),
+      'urlTo': '/order-details'
+    },
+    {
+      'label': 'Create Order Details',
+      'active': true
+    }
+    ];
   }
 
+  onChange(changes: any) {
+    this.model = changes;
+
+    this.saveModel();
+  }
+
+  saveModel() {
+    this.model.save(this.dbService)
+      .subscribe((key: any) => {
+        this.model = null;
+        this.model = new OrderDetails();
+        console.log('key:');
+        console.log(key);
+      })
+  }
 }
