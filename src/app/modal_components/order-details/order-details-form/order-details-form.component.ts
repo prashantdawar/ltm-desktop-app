6e6b09ede1e000b271349861870bb2c4368490cc
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-20 20:42:43 +0530

    updated: order-details component

--- src/app/modal_components/order-details/order-details-form/order-details-form.component.ts
+++ src/app/modal_components/order-details/order-details-form/order-details-form.component.ts
@@ -1,21 +1,32 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrderDetails } from 'src/app/modals_data/order_details';
+
+
+
+
+
+
 
 @Component({
   selector: 'app-order-details-form',
   templateUrl: './order-details-form.component.html',
+
   styleUrls: ['./order-details-form.component.css']
 })
-
 
 
 export class OrderDetailsFormComponent implements OnInit {
   @Input() model: OrderDetails = new OrderDetails;
   @Output() modelChange = new EventEmitter<OrderDetails>();
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm){
+
+  }
 }
--- src/app/modals_data/order_details.ts
+++ src/app/modals_data/order_details.ts
@@ -5,22 +5,22 @@ export class OrderDetails {
     public order_total!: number;
     public order_discount!: number;
     public order_discount_percentage!: number;
-    
-    
-    
+
+
+
     public order_total_payable!: number;
     public order_payment_method!: number;
     public order_status_enable!: number;
-    
+
     public order_status_delivery!: number;
     public order_customer_id!: number;
-    
-    
+
+
     public order_customer_uid!: number;
     public order_customer_name!: string;
     public order_customer_company_name!: string;
     public order_discount_coupon!: number;
-    
+
     public order_referrer!: string;
     public order_notes!: string;
     public order_notes_internal!: string;
@@ -29,21 +29,29 @@ export class OrderDetails {
     public order_payment_uid!: number;
     public order_confirmation_send!: number;
     public order_confirmation_send_sms!: number;
-    
+
     public order_confirmation_send_call!: number;
     public order_confirmation_send_email!: number;
     public order_confirmation_send_whatsapp!: number;
     public order_confirmation_send_object!: string;
     public order_event_stream!: string;
-    
+
     public update_log!: string;
     public order_email_log!: string;
-    
 
-    getStoreName() { return 'order_details'};
+    rules() {
+        return []
+    }
+
+    attributeLabels(){
+        return {
+            'order_customer_id': 'Customer ID'
+        }
+    }
+    getStoreName() { return 'order_details' };
     getClassName() { return 'OrderDetails' };
     getIndexKey() { return this.order_id };
-    
+
     getClassLabel() {
         let className = this.getClassName();
         return className.split(/(?=[A-Z])/).join(" ");
