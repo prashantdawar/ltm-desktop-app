bf9bc1f04de286d56639e4d282c50a9ada0c0e58
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-20 19:51:38 +0530

    updated

--- src/app/modal_components/order-details/order-details-form/order-details-form.component.ts
+++ src/app/modal_components/order-details/order-details-form/order-details-form.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { OrderDetails } from 'src/app/modals_data/order_details';
 
 @Component({
   selector: 'app-order-details-form',
   templateUrl: './order-details-form.component.html',
   styleUrls: ['./order-details-form.component.css']
 })
+
+
+
 export class OrderDetailsFormComponent implements OnInit {
+  @Input() model: OrderDetails = new OrderDetails;
+  @Output() modelChange = new EventEmitter<OrderDetails>();
 
   constructor() { }
 
