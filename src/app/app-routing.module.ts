6fbc2b242362152c1ac74102899e1a576916ce99
prashantdawar <prashantdawar.2008@gmail.com>
2022-04-20 19:34:57 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,44 +10,52 @@ import { CustomerDetailsIndexComponent } from './modal_components/customer-detai
 
 import { CustomerDetailsUpdateComponent } from './modal_components/customer-details/customer-details-update/customer-details-update.component';
 import { CustomerDetailsViewComponent } from './modal_components/customer-details/customer-details-view/customer-details-view.component';
-import { ProductCategoryDetailsCreateComponent } from './modal_components/product-category-details/product-category-details-create/product-category-details-create.component';
+import { OrderDetailsCreateComponent } from './modal_components/order-details/order-details-create/order-details-create.component';
 
+import { OrderDetailsIndexComponent } from './modal_components/order-details/order-details-index/order-details-index.component';
+import { OrderDetailsUpdateComponent } from './modal_components/order-details/order-details-update/order-details-update.component';
+
+
+import { OrderDetailsViewComponent } from './modal_components/order-details/order-details-view/order-details-view.component';
+import { ProductCategoryDetailsCreateComponent } from './modal_components/product-category-details/product-category-details-create/product-category-details-create.component';
 import { ProductCategoryDetailsIndexComponent } from './modal_components/product-category-details/product-category-details-index/product-category-details-index.component';
 import { ProductCategoryDetailsUpdateComponent } from './modal_components/product-category-details/product-category-details-update/product-category-details-update.component';
-
 
 import { ProductCategoryDetailsViewComponent } from './modal_components/product-category-details/product-category-details-view/product-category-details-view.component';
 import { ProductDetailsFormComponent } from './modal_components/product-details/product-details-form/product-details-form.component';
 import { ProductDetailsIndexComponent } from './modal_components/product-details/product-details-index/product-details-index.component';
 
-
 const routes: Routes = [
   { path: '', component: AppComponent },
+
+
+
+  { path: 'order-details', component: OrderDetailsIndexComponent },
+  { path: 'order-details/create', component: OrderDetailsCreateComponent },
+  { path: 'order-details/update/:orderId', component: OrderDetailsUpdateComponent },
+  { path: 'order-details/view/:orderId', component: OrderDetailsViewComponent },
+
 
   { path: 'customer-details', component: CustomerDetailsIndexComponent },
   { path: 'customer-details/create', component: CustomerDetailsCreateComponent },
   { path: 'customer-details/view/:customerId', component: CustomerDetailsViewComponent },
   { path: 'customer-details/update/:customerId', component: CustomerDetailsUpdateComponent },
 
-  
   { path: 'product-details', component: ProductDetailsIndexComponent },
   { path: 'product-details/create', component: ProductDetailsFormComponent },
 
-  
-  
-  
   { path: 'product-category-details', component: ProductCategoryDetailsIndexComponent },
   { path: 'product-category-details/create', component: ProductCategoryDetailsCreateComponent },
   { path: 'product-category-details/update/:productCategoryId', component: ProductCategoryDetailsUpdateComponent },
+
   { path: 'product-category-details/view/:productCategoryId', component: ProductCategoryDetailsViewComponent },
-  
+
   { path: 'coupon-details', component: CouponDetailsIndexComponent },
   { path: 'coupon-details/create', component: CouponDetailsFormComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  
   exports: [RouterModule]
 })
 
---
+++ src/app/modals_data/order_details.ts
@@ -0,0 +1,42 @@
+export class OrderDetails {
+    public order_id!: number;
+    public order_uid!: number;
+    public order_details_object!: string;
+    public order_total!: number;
+    public order_discount!: number;
+    public order_discount_percentage!: number;
+    
+    
+    
+    public order_total_payable!: number;
+    public order_payment_method!: number;
+    public order_status_enable!: number;
+    
+    public order_status_delivery!: number;
+    public order_customer_id!: number;
+    
+    
+    public order_customer_uid!: number;
+    public order_customer_name!: string;
+    public order_customer_company_name!: string;
+    public order_discount_coupon!: number;
+    
+    public order_referrer!: string;
+    public order_notes!: string;
+    public order_notes_internal!: string;
+    public order_gift_message!: string;
+    public order_payment_id!: number;
+    public order_payment_uid!: number;
+    public order_confirmation_send!: number;
+    public order_confirmation_send_sms!: number;
+    
+    public order_confirmation_send_call!: number;
+    public order_confirmation_send_email!: number;
+    public order_confirmation_send_whatsapp!: number;
+    public order_confirmation_send_object!: string;
+    public order_event_stream!: string;
+    
+    public update_log!: string;
+    public order_email_log!: string;
+    
+}
